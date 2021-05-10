b1acba1ee209caec8f8cc203e3530fc5bb838f19
Melanie Lucht <melanie.lucht@th-luebeck.de>
2021-05-10 15:00:00 +0200

    logfile hack

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,16 @@ import { environment } from './environments/environment';
 
 if (environment.production) {
   enableProdMode();
+
+  // No console.log in production
+  // This is the hacky way (-> LogService)
+  if (environment.logFiles == false && window) {
+    window.console.log = window.console.warn = window.console.info = function () {
+      // Don't log anything.
+    };
+  }
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
+  .catch((err) => console.error(err));
