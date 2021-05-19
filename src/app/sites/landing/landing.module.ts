fffe2762cf479894fb3dbb565d7fbcc5cd05ef02
Melanie Lucht <melanie.lucht@th-luebeck.de>
2021-05-19 17:31:48 +0200

    CourseCarousel has its own module

--- src/app/sites/landing/landing.module.ts
+++ src/app/sites/landing/landing.module.ts
@@ -1,24 +1,15 @@
-import { Injectable, NgModule } from '@angular/core';
-import { HAMMER_GESTURE_CONFIG, HammerGestureConfig, HammerModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { SharedModule } from 'src/app/shared/shared.module';
-import * as Hammer from 'hammerjs';
 
 import { LandingComponent } from 'src/app/sites/landing/pages/landing/landing.components';
 import { AdvantagesComponent } from 'src/app/sites/landing/components/advantages/advantages.component';
 import { PreviewInfoBannerComponent } from 'src/app/sites/landing/components/banner-top/banner-top.component';
-import { CoursecarouselComponent } from 'src/app/sites/landing/components/coursecarousel/coursecarousel.component';
 import { HerocontainerComponent } from 'src/app/sites/landing/components/herocontainer/herocontainer.component';
 import { PartnershipComponent } from 'src/app/sites/landing/components/partnership/partnership.component';
 import { TrailerComponent } from 'src/app/sites/landing/components/trailer/trailer.component';
 import { ConsentVideoComponent } from 'src/app/sites/landing/components/trailer//consent-video/consent-video.component';
-
-@Injectable()
-export class HammerConfig extends HammerGestureConfig {
-  overrides = {
-    swipe: { direction: Hammer.DIRECTION_ALL },
-  };
-}
+import { CoursecarouselModule } from 'src/app/sites/landing/components/coursecarousel/coursecarousel.module';
 
 @NgModule({
   declarations: [
@@ -29,19 +20,9 @@ export class HammerConfig extends HammerGestureConfig {
     PartnershipComponent,
     TrailerComponent,
     ConsentVideoComponent,
-    CoursecarouselComponent
   ],
-  imports:[
-    RouterModule,
-    SharedModule,
-    HammerModule,
-  ],
-  exports:[],
-  providers: [{
-    provide: HAMMER_GESTURE_CONFIG,
-    useClass: HammerConfig
-  }
-  ]
+  imports: [RouterModule, SharedModule, CoursecarouselModule],
+  exports: [],
+  providers: [],
 })
-
 export class LandingPageModule {}
