ab828eb123c32bd0fbee56d1740daa8e6a8c6af4
arjanvanzwol <arjanvanzwol@googlemail.com>
2021-10-29 11:22:13 +0200

    - landing module: removed herocontainer + trailer component + preview info banner component - added new bg image (2500px) - integrated image in css code with new media query (>1800px)

--- src/app/sites/landing/landing.module.ts
+++ src/app/sites/landing/landing.module.ts
@@ -4,10 +4,7 @@ import { SharedModule } from 'src/app/shared/shared.module';
 
 import { LandingComponent } from 'src/app/sites/landing/pages/landing/landing.components';
 import { AdvantagesComponent } from 'src/app/sites/landing/components/advantages/advantages.component';
-import { PreviewInfoBannerComponent } from 'src/app/sites/landing/components/banner-top/banner-top.component';
-import { HerocontainerComponent } from 'src/app/sites/landing/components/herocontainer/herocontainer.component';
 import { PartnershipComponent } from 'src/app/sites/landing/components/partnership/partnership.component';
-import { TrailerComponent } from 'src/app/sites/landing/components/trailer/trailer.component';
 import { CoursecarouselModule } from 'src/app/sites/landing/components/coursecarousel/coursecarousel.module';
 import { KiPlaygroundBlockComponent } from './components/ki-playground-block/ki-playground-block.component';
 import { RoundEasyButtonComponent } from './components/ki-playground-block/round-easy-button/round-easy-button.component';
@@ -17,10 +14,7 @@ import { IntroBannerComponent } from './components/intro-banner/intro-banner.com
   declarations: [
     LandingComponent,
     AdvantagesComponent,
-    PreviewInfoBannerComponent,
-    HerocontainerComponent,
     PartnershipComponent,
-    TrailerComponent,
     KiPlaygroundBlockComponent,
     RoundEasyButtonComponent,
     IntroBannerComponent
