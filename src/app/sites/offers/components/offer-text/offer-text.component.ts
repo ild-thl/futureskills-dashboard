3f13bff1da1334e83a1cff8dea2ffc9654b671eb
arjanvanzwol <arjanvanzwol@googlemail.com>
2022-02-07 13:45:41 +0100

    - added html code to the text component - added scss code the scss component file - added offer data to the ts file - added scss code to the recommended component - replaced html with app selector in offer detail - removed the scss code from the offer detail (recommended + text)

--- src/app/sites/offers/components/offer-text/offer-text.component.ts
+++ src/app/sites/offers/components/offer-text/offer-text.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Offer } from 'src/app/core/models/offer';
+import { Subscription } from 'rxjs';
+import { User } from 'src/app/core/models/user';
+import { ErrorHandlerService } from 'src/app/core/services/error-handling/error-handling';
+import { OfferDataService } from 'src/app/core/data/offer/offer-data.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-offer-text',
@@ -6,10 +12,50 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./offer-text.component.scss']
 })
 export class OfferTextComponent implements OnInit {
+  private userSub: Subscription;
+  private paramsSub: Subscription;
 
-  constructor() { }
+  public offer: Offer;
+  public user: User;
+  public isLoading = false;
+  public subscribed = false;
 
-  ngOnInit(): void {
+  isError: boolean;
+  errMessage: string;
+
+  constructor(
+    private offerDataService: OfferDataService,
+    private route: ActivatedRoute,
+    private errorHandler: ErrorHandlerService
+  ) { }
+
+  ngOnInit() {
+    this.paramsSub = this.route.paramMap.subscribe((params) => {
+      const offerId = +params.get('id');
+
+      this.isLoading = true;
+      this.errMessage = '';
+      this.isError = false;
+
+      this.userSub = this.offerDataService.getOfferDataForDetail(offerId).subscribe({
+        next: (data) => {
+          //this.logService.log('offer-detail', 'Detaildata', data);
+          this.user = data.user;
+          this.offer = data.offerData;
+          this.subscribed = false;
+          this.isLoading = false;
+          this.errMessage = '';
+          this.isError = false;
+        },
+        error: (error: Error) => {
+          this.isLoading = false;
+          this.isError = true;
+          //this.errMessage =this.errorHandler.getErrorMessage(error, 'offer');
+          // Besser immer die not-found-Fehlermeldung anzeigen.
+          this.errMessage = this.errorHandler.ERROR_MESSAGES.E404_OFFER_NOT_FOUND;
+        },
+      });
+    });
   }
 
 }
