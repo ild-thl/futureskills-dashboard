1f8e2bbcbbc009e0352d11ad5cdade4567801c76
arjanvanzwol <arjanvanzwol@googlemail.com>
2022-02-08 13:21:16 +0100

    - changed recommended ts to @input() and removed the other code - binded the offer property into the recommended selector on the parent

--- src/app/sites/offers/components/recommended/recommended.component.ts
+++ src/app/sites/offers/components/recommended/recommended.component.ts
@@ -1,61 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { Offer } from 'src/app/core/models/offer';
-import { Subscription } from 'rxjs';
-import { User } from 'src/app/core/models/user';
-import { ErrorHandlerService } from 'src/app/core/services/error-handling/error-handling';
-import { OfferDataService } from 'src/app/core/data/offer/offer-data.service';
-import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-recommended',
   templateUrl: './recommended.component.html',
   styleUrls: ['./recommended.component.scss']
 })
-export class RecommendedComponent implements OnInit {
-  private userSub: Subscription;
-  private paramsSub: Subscription;
-
-  public offer: Offer;
-  public user: User;
-  public isLoading = false;
-  public subscribed = false;
-
-  isError: boolean;
-  errMessage: string;
-
-  constructor(
-    private offerDataService: OfferDataService,
-    private route: ActivatedRoute,
-    private errorHandler: ErrorHandlerService
-  ) { }
-
-  ngOnInit() {
-    this.paramsSub = this.route.paramMap.subscribe((params) => {
-      const offerId = +params.get('id');
-
-      this.isLoading = true;
-      this.errMessage = '';
-      this.isError = false;
-
-      this.userSub = this.offerDataService.getOfferDataForDetail(offerId).subscribe({
-        next: (data) => {
-          //this.logService.log('offer-detail', 'Detaildata', data);
-          this.user = data.user;
-          this.offer = data.offerData;
-          this.subscribed = false;
-          this.isLoading = false;
-          this.errMessage = '';
-          this.isError = false;
-        },
-        error: (error: Error) => {
-          this.isLoading = false;
-          this.isError = true;
-          //this.errMessage =this.errorHandler.getErrorMessage(error, 'offer');
-          // Besser immer die not-found-Fehlermeldung anzeigen.
-          this.errMessage = this.errorHandler.ERROR_MESSAGES.E404_OFFER_NOT_FOUND;
-        },
-      });
-    });
-  }
-
+export class RecommendedComponent {
+  @Input() offer: Offer;
 }
