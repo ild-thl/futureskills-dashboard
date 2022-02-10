f83a4d252e07aa6b155fdfde120bdd5539a3d046
arjanvanzwol <arjanvanzwol@googlemail.com>
2022-02-10 17:47:01 +0100

    - added headline text over @Input

--- src/app/shared/components/headline-banner/headline-banner.component.ts
+++ src/app/shared/components/headline-banner/headline-banner.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'fs-headline-banner',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./headline-banner.component.scss']
 })
 export class HeadlineBannerComponent implements OnInit {
+
+  @Input() headline: string;
 
   constructor() { }
 
