0fb26f0166c6a22ca0503fac05cc692f21424d33
arjanvanzwol <arjanvanzwol@googlemail.com>
2022-02-21 11:47:59 +0100

    removed constructor + ngoninit from ts file

--- src/app/sites/info-teaching/components/video-presentation/video-presentation.component.ts
+++ src/app/sites/info-teaching/components/video-presentation/video-presentation.component.ts
@@ -1,15 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'fs-video-presentation',
   templateUrl: './video-presentation.component.html',
   styleUrls: ['./video-presentation.component.scss']
 })
-export class VideoPresentationComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class VideoPresentationComponent {
 }
