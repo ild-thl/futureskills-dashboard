84109199958b77dd26718cb2da04d4d833395478
Melanie Lucht <melanie.lucht@th-luebeck.de>
2022-04-08 12:20:14 +0200

    clean-up

--- src/app/core/guards/auth-guard.ts
+++ src/app/core/guards/auth-guard.ts
@@ -7,9 +7,7 @@ import {
 } from '@angular/router';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { AuthService } from 'src/app/core/auth/auth.service';
 import { StaticService } from 'src/app/config/static.service';
-import { PermissionService } from 'src/app/core/services/permissions/permission.service';
 import { TokenService } from 'src/app/core/services/token-check/token.service';
 
 @Injectable({ providedIn: 'root' })
@@ -17,10 +15,8 @@ export class AuthGuard implements CanLoad {
   lnkLogin = this.staticConfig.getPathInfo().lnkLogin;
   lnkNotAllowed = this.staticConfig.getPathInfo().lnkNotAllowed;
   constructor(
-    private authService: AuthService,
     private router: Router,
     private staticConfig: StaticService,
-    private permissionService: PermissionService,
     private tokenService: TokenService,
   ) {}
   canLoad(route: Route, segments: UrlSegment[]): boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
